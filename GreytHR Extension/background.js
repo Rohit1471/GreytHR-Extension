f9a6c6f76375702875751f840b73f9ac7fa7c563
Rohit Pathak <66103020+Rohit1471@users.noreply.github.com>
2024-11-29 14:51:40 +0530

    Update Calling Logic For Chrome's event listener for API
    Stored API local and instead of getting API by listening chrome's event listener we just store it locally once after getting it and than change the date in the endpoint as per respective dates.

--- GreytHR Extension/background.js
+++ GreytHR Extension/background.js
@@ -1,13 +1,36 @@
-chrome.webRequest.onCompleted.addListener(
-    function (details) {
-      const urlPattern = /^https:\/\/designnbuy\.greythr\.com\/latte\/v3\/attendance\/info\/\d+\/swipes(\?.*)?$/;
-      if (details.url.includes('https://designnbuy.greythr.com/latte/v3/attendance/info/') && urlPattern.test(details.url)) {
-        chrome.storage.local.set({ apiData: details.url });
-        console.log('Set Url : ', details.url);
-      }else{
-        console.log("No Url matched....")
-      }
-    },
-    { urls: ["<all_urls>"] } // Adjust to match API URL
-  );
-  
+let checkIfHasLocalUrl;
+
+function checkIfApiIsInLocalAsync() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get("apiData", function (result) {
+      result.apiData ? resolve(result.apiData) : reject("No Api Data In Local");
+    });
+  });
+}
+
+async function checkIfAPIInLocal(){
+  try{
+    checkIfHasLocalUrl = await checkIfApiIsInLocalAsync();
+    console.log("checkIfHasLocalUrl: ", checkIfHasLocalUrl);
+  } catch(error){
+    if (!checkIfHasLocalUrl) {
+      chrome.webRequest.onCompleted.addListener(
+        function (details) {
+          // console.log("details.url: ", details.url);
+          const urlPattern =/^https:\/\/designnbuy\.greythr\.com\/latte\/v3\/attendance\/info\/\d+\/swipes(\?.*)?$/;
+          if (details.url.includes("https://designnbuy.greythr.com/latte/v3/attendance/info/") && urlPattern.test(details.url)) {
+            chrome.storage.local.set({ apiData: details.url });
+            console.log("Set Url : ", details.url);
+          } else {
+            console.log("No Url matched....");
+          }
+        },
+        { urls: ["<all_urls>"] } // Adjust to match API URL
+      );
+    }
+  }
+}
+
+checkIfAPIInLocal();
+
+
